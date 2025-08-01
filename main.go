cbd32f44e2c559b2d77d7b39e717c4ab7aaec774
Viacheslav Korobeynikov <korobeynikovvo@gmail.com>
2025-08-01 08:51:39 +0300

    Разделение логики на функции

--- main.go
+++ main.go
@@ -8,14 +8,29 @@ import (
 const IMTPower = 2
 
 func main() {
+	fmt.Println("___Калькулятор индекса массы тела___")
+	userKg, userHeight := getUserInput()
+	IMT := calculateIMT(userKg, userHeight)
+	outputResult(IMT)
+}
+
+func outputResult(imt float64) {
+	result := fmt.Sprintf("Ваш индекс массы тела составляет: %.0f", imt)
+	fmt.Print(result)
+}
+
+func calculateIMT(userKg float64, userHeight float64) float64 {
+	IMT := userKg / math.Pow(userHeight/100, IMTPower)
+	return IMT
+}
+
+func getUserInput() (float64, float64) {
 	var userHeight float64
 	var userKg float64
-	fmt.Println("___Калькулятор индекса массы тела___")
-	fmt.Println("Введите свой рост (в метрах): ")
+	fmt.Println("Введите свой рост (в сантиметрах): ")
 	fmt.Scan(&userHeight)
 	fmt.Println("Введите свой вес (в кг): ")
 	fmt.Scan(&userKg)
-	IMT := userKg / math.Pow(userHeight, 2)
-	fmt.Println("Ваш индекс массы тела составляет: ")
-	fmt.Print(IMT)
+	return userKg, userHeight
+
 }
